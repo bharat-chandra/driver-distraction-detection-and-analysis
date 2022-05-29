bbed411685bc649c36109cf0473f20709795799f
bharat-chandra <53616992+bharat-chandra@users.noreply.github.com>
2022-05-29 16:03:44 +0530

    Delete driver distraction/webapp directory

--- driver distraction/webapp/firstApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Performance
-
-admin.site.register(Performance)
--- driver distraction/webapp/firstApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Performance(models.Model):
-    driver_id = models.IntegerField()
-    date = models.TextField()
-    timestamp = models.TextField()
-    class Meta:
-        db_table = 'Performance'
